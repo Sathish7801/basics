9f548f2a3e512e4f9c083040ac9e98da35b0ee6e
MIT <MIT@MVITECESIMLAB-10>
2024-06-27 12:08:41 +0530

    datatype

---
+++ newl/src/datatype/DataType.java
@@ -0,0 +1,21 @@
+package datatype;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		int a = 7/3;
+		float b = 90/7;
+		double c = 6d/4d;
+		float d = 10/3.5f;
+		System.out.println("value1:" +a);
+		System.out.println("value2:" +b);
+		System.out.println("value3:" +c);
+		System.out.println("value4:" +d);
+		
+		int value = 350;
+		double percentage = value * 0.46;
+		System.out.println("percentage:" +percentage);
+
+	}
+
+}
