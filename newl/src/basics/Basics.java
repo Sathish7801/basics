a26c6497084c937eecf316f433c25ebbea0b3172
MIT <MIT@MVITECESIMLAB-10>
2024-06-27 11:05:15 +0530

    This is basics

---
+++ newl/src/basics/Basics.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("Bye");
+		System.out.print("Welcome");
+
+	}
+
+}
