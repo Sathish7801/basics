ebab8742a2dea7243316d8887c460a5e443e3928
MIT <MIT@MVITECESIMLAB-10>
2024-06-27 11:41:57 +0530

    character representation

---
+++ newl/src/day1/CharacterrRep.java
@@ -0,0 +1,20 @@
+package day1;
+
+public class CharacterrRep {
+
+	public static void main(String[] args) {
+		char var= 'c';
+		System.out.println(var);
+		char var1=67;
+		System.out.println(var1);
+		char var2 = '\u0023';
+		System.out.println(var2);
+		char var3 = '\u0021';
+		System.out.println(var3);
+		int a = 'A';
+		System.out.println(a);
+		
+
+	}
+
+}
