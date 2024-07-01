b502cbd5e82404eacf6f2ccb40b03c70d78d185a
MIT <MIT@MVITECESIMLAB-10>
2024-07-01 14:51:14 +0530

    day 5 updated

---
+++ newl/src/basics/DoubleJeopardy.java
@@ -0,0 +1,11 @@
+package basics;
+
+public class DoubleJeopardy {
+
+	public static void main(String[] args) {
+		double value = 32;
+		System.out.println("A Double:" +value);
+
+	}
+
+}
---
+++ newl/src/basics/Username.java
@@ -0,0 +1,12 @@
+package basics;
+import java.util.Scanner;
+public class Username {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		System.out.print("Your name is " +str);
+
+	}
+
+}
