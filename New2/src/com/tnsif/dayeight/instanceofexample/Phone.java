4e2c7ed1f4c12a48486fc36c707cd0af33aa1909
hp <hp@192.168.56.1>
2024-07-14 12:08:50 +0530

    day eight update

---
+++ New2/src/com/tnsif/dayeight/instanceofexample/Phone.java
@@ -0,0 +1,9 @@
+package com.tnsif.dayeight.instanceofexample;
+
+public interface Phone {
+	
+	void call();
+
+	void sms();
+
+}
---
+++ New2/src/com/tnsif/dayeight/instanceofexample/PhoneFactory.java
@@ -0,0 +1,13 @@
+package com.tnsif.dayeight.instanceofexample;
+
+public class PhoneFactory {
+	
+	public static Phone createPhone(String company) {
+		if (company.equalsIgnoreCase("Samsung"))
+			return new Samsung();
+		else if (company.equalsIgnoreCase("Jio"))
+			return new Jio();
+		return null;
+	}
+
+}
